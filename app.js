2ad16fe75db8d1e772dfc89b69f4270877e0d0ea
kmrajibfaysal <kmrajibfaysal@gmail.com>
2024-08-18 03:49:48 +0600

    added all joke loader api files and link up with a button

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('hello');
+const getJoke = async () => {
+  try {
+    const config = { headers: { Accept: 'application/json' } };
+    const res = await axios.get('https://icanhazdadjoke.com/', config);
+    return res.data.joke;
+  } catch (e) {
+    throw e;
+  }
+};
+
+// getDadJoke();
+const button = document.querySelector('button');
+const joke_container = document.querySelector('#joke_container');
+
+button.addEventListener('click', async () => {
+  const newJoke = await getJoke();
+  const newLi = document.createElement('li');
+  newLi.textContent = newJoke;
+  joke_container.append(newLi);
+});
